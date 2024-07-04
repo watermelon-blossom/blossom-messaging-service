ed97fb0e304f21e4c59cbb645240dc0394a3df91
양준혁 <highestbright98@naver.com>
2024-07-04 22:33:15 +0900

    project setting. over

---
+++ src/test/java/com/watermelon/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watermelon.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
