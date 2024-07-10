eb3aec898d54219a667df7dded2d3bd1a834c26d
양준혁 <highestbright98@naver.com>
2024-07-10 17:04:53 +0900

    [KAN-87] setter 제거

--- src/main/java/com/watermelon/message/domain/chat/Chat.java
+++ src/main/java/com/watermelon/message/domain/chat/Chat.java
@@ -9,13 +9,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Document(collection = "chat")
 @Getter
 @ToString
-@Setter
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
--- src/main/java/com/watermelon/message/domain/chat/ChatRoom.java
+++ src/main/java/com/watermelon/message/domain/chat/ChatRoom.java
@@ -6,13 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Document(collection = "chatRoom")
 @Getter
 @ToString
-@Setter
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
