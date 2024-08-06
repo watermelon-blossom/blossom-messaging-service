167e42fbe5c2de616be075ce905fbcc0b192d1d0
양준혁 <highestbright98@naver.com>
2024-08-06 11:11:27 +0900

    [KAN-133] 카프카 설정 완료

--- src/main/java/com/watermelon/message/infra/KafkaProducerConfiguration.java
+++ src/main/java/com/watermelon/message/infra/KafkaProducerConfiguration.java
@@ -3,6 +3,7 @@ package com.watermelon.message.infra;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import com.google.common.collect.ImmutableMap;
-import com.watermelon.message.dto.chat.SendChatResponse;
+import com.watermelon.message.dto.chat.ChatResponse;
 
 @EnableKafka
 @Configuration
@@ -25,34 +26,26 @@ public class KafkaProducerConfiguration {
 	@Value("${spring.kafka.consumer.group-id}")
 	private String groupId;
 
-	@Value("${spring.kafka.consumer.key-deserializer}")
-	private String keyDeserializer;
-
 	// Kafka ProducerFactory를 생성하는 Bean 메서드
 	@Bean
-	public ProducerFactory<String, SendChatResponse> producerFactory() {
+	public ProducerFactory<String, ChatResponse> producerFactory() {
 		return new DefaultKafkaProducerFactory<>(producerConfigurations());
 	}
 
 	// Kafka Producer 구성을 위한 설정값들을 포함한 맵을 반환하는 메서드
 	@Bean
 	public Map<String, Object> producerConfigurations() {
-		JsonDeserializer<SendChatResponse> deserializer = new JsonDeserializer<>(SendChatResponse.class);
-		deserializer.setRemoveTypeHeaders(false);
-		deserializer.addTrustedPackages("*");
-		deserializer.setUseTypeMapperForKey(true);
-
 		return ImmutableMap.<String, Object>builder()
 			.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer)
 			.put("group.id", groupId)
-			.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keyDeserializer)
-			.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, deserializer)
+			.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+			.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonDeserializer.class)
 			.build();
 	}
 
 	// KafkaTemplate을 생성하는 Bean 메서드
 	@Bean
-	public KafkaTemplate<String, SendChatResponse> kafkaTemplate() {
+	public KafkaTemplate<String, ChatResponse> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
 	}
 }
