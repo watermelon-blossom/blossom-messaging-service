fb20a699f9186a62c112a4cf5a1b14281708983e
양준혁 <highestbright98@naver.com>
2024-08-19 01:45:21 +0900

    [KAN-123] 웹 스톰프 연결/테스트

---
+++ src/main/java/com/watermelon/message/application/ChatReadService.java
@@ -0,0 +1,34 @@
+package com.watermelon.message.application;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import com.watermelon.message.domain.mongo.chat.Chat;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatReadService {
+	private final MongoTemplate mongoTemplate;
+
+	public void readRoom(String roomId, String userId) {
+		// 조건을 정의: roomId가 일치하고, senderId가 userId와 다르며, hasRead가 false인 문서
+		Query query = new Query();
+		query.addCriteria(Criteria.where("roomId").is(roomId)
+			.and("senderId").ne(userId)
+			.and("hasRead").is(false));
+
+		// 업데이트할 내용 정의: hasRead를 true로 설정
+		Update update = new Update();
+		update.set("hasRead", true);
+
+		// 조건에 맞는 문서들을 한 번에 업데이트
+		mongoTemplate.updateMulti(query, update, Chat.class);
+	}
+}
