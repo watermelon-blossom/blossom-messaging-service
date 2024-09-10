25b4fc633cedbcb814cdfdb32330079b07fee311
uicheon <highestbright98@naver.com>
2024-09-10 20:44:25 +0900

    exclude redis auto configure

--- src/main/java/com/watermelon/chat/MessageApplication.java
+++ src/main/java/com/watermelon/chat/MessageApplication.java
@@ -2,8 +2,11 @@ package com.watermelon.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		RedisAutoConfiguration.class,
+})
 public class MessageApplication {
 
 	public static void main(String[] args) {
